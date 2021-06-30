885a2fc2d8cff8d32ec997ff1002634b2d94cd50
anishatabsa <anish.kaithassery@takealot.com>
2021-06-30 22:28:30 +0200

    Removed test files

--- tests/fixtures.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from business_seconds.controllers import business_seconds_controller as c
-
-
-@pytest.fixture(scope='module')
-def controller(request):
-    controller = c.Controller()
-    yield controller
-    pass
